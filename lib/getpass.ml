ece77421f5180ac6b2b1e92e555326dd475102a4
lmenou <menl94629@gmail.com>
2024-05-24 16:18:20 +0200

    feat: read password invisibly?

--- lib/getpass.ml
+++ lib/getpass.ml
@@ -1,15 +1,17 @@
-let nshow () =
-  let buf = Input.create () in
-  let rec accumulate () =
-    let ch = Char.code (input_char stdin) in
-    match ch with
-    | 10 -> Input.get buf
-    | code ->
-        Input.add_char buf (Char.chr code);
-        accumulate ()
-  in
-  accumulate ()
+let tty_set_echo value =
+  let module U = Unix in
+  let fd = U.openfile "/dev/tty" [ U.O_RDWR ] 0o777 in
+  let termio = U.tcgetattr fd in
+  termio.c_echo <- value;
+  U.tcsetattr fd U.TCSADRAIN termio;
+  U.close fd
+
+let read_pass () =
+  tty_set_echo false;
+  let pass = read_line () in
+  tty_set_echo true;
+  pass
 
 let getpass message =
   print_endline message;
-  nshow ()
+  read_pass ()
