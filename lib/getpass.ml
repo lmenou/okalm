73bf61dd78f3ec164bad290761c2d48dfc6745e8
lmenou <menl94629@gmail.com>
2024-05-25 01:36:37 +0200

    refactor: rename set_tty_echo

--- lib/getpass.ml
+++ lib/getpass.ml
@@ -1,4 +1,4 @@
-let tty_set_echo value =
+let tty_set_echo_to value =
   let module U = Unix in
   let fd = U.openfile "/dev/tty" [ U.O_RDWR ] 0o777 in
   let termio = U.tcgetattr fd in
@@ -7,9 +7,9 @@ let tty_set_echo value =
   U.close fd
 
 let read_pass () =
-  tty_set_echo false;
+  tty_set_echo_to false;
   let pass = read_line () in
-  tty_set_echo true;
+  tty_set_echo_to true;
   pass
 
 let getpass message =
