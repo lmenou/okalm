ba266977650ee10702566aa863d7ed887504d69c
lmenou <menl94629@gmail.com>
2024-05-24 16:34:19 +0200

    feat: abort cowardly on windows

--- lib/crypter.ml
+++ lib/crypter.ml
@@ -1,6 +1,18 @@
+let is_host_windows () =
+  match Sys.os_type with
+  | "Win32" ->
+      print_endline
+        "Warning: the CLI is not battery tested on Windows, you may experience \
+         violent bugs, aborting cowardly";
+      true
+  | _ -> false
+
 let crypter file =
-  print_endline file;
-  let pass = Getpass.getpass "Please enter your password:" in
-  print_endline pass;
-  let hash = Bcrypt.hash pass in
-  hash |> Bcrypt.string_of_hash |> print_endline
+  match is_host_windows () with
+  | true -> ()
+  | false ->
+      print_endline file;
+      let pass = Getpass.getpass "Please enter your password:" in
+      print_endline pass;
+      let hash = Bcrypt.hash pass in
+      hash |> Bcrypt.string_of_hash |> print_endline
