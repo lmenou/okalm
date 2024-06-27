56fc3f63a56a6223209c3144494781add0a55c8a
lmenou <menl94629@gmail.com>
2024-06-27 10:53:04 +0200

    feat: encrypt larger file

--- lib/encwrite.ml
+++ lib/encwrite.ml
@@ -1,3 +1,6 @@
+let step = 70
+let max_size = Int64.of_int step
+
 (** [write file content] overwrite [content] in [file] *)
 let write filename content =
   let module O = Out_channel in
@@ -16,9 +19,35 @@ let encrypt content key iv =
   let _ = aest#finish_and_get_tag in
   aest#get_string
 
+let encryptf file key iv =
+  let module I = In_channel in
+  let module O = Out_channel in
+  let rec crypting ic oc key iv pos =
+    let filesize = (Unix.LargeFile.stat file).st_size in
+    if Int64.add pos max_size >= filesize then
+      let maxb = Int64.to_int @@ Int64.sub filesize pos in
+      let _ = I.seek ic pos in
+      let buffer = Bytes.create maxb in
+      let _ = I.input ic buffer 0 maxb in
+      let to_write = encrypt (Bytes.to_string buffer) key iv in
+      let _ = O.output_string oc to_write in
+      let _ = I.close ic in
+      O.close oc
+    else
+      let buffer = Bytes.create step in
+      let _ = I.seek ic pos in
+      let _ = I.input ic buffer 0 step in
+      let to_write = encrypt (Bytes.to_string buffer) key iv in
+      let _ = O.output_string oc to_write in
+      crypting ic oc key iv (Int64.add pos max_size)
+  in
+  let ic = I.open_bin file in
+  let oc = O.open_gen [ Open_creat; Open_append ] 0o644 "coucou" in
+  crypting ic oc key iv (Int64.of_int 0)
+
 let crypt file key iv =
   let filesize = (Unix.LargeFile.stat file).st_size in
-  if filesize > Int64.of_int 70 then print_endline "not now"
+  if filesize > max_size then encryptf file key iv
   else
     let content = read file in
     let result = encrypt content key iv in
